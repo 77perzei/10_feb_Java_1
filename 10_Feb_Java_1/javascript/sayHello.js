30cedef7f09c9a07f46880f35e6335fda60eae60
Per Zeilon <per.zeilon@gmail.com>
2021-02-10 15:01:55 +0100

    10_feb_15_00_commit

---
+++ 10_Feb_Java_1/javascript/sayHello.js
@@ -0,0 +1,16 @@
+
+const greetUser = 'Heyu Welcome yo my site' //String
+const age = 42 //Number
+const x = undefined //Inget värde angett
+const isTeatcher = 'true' //Boolean
+const isNurse = 'false' //Boolean
+
+
+
+
+
+var firstname = 'Per'
+
+function sayHello() {
+    alert(sayHello)
+}
